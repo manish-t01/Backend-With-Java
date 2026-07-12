98668a8af6547a909edd49556907620a7278684a
manish-t01 <manish9366801212@gmail.com>
2026-07-13 00:53:22 +0530

    InsertDemo_03

--- JavaCodes/src/in/sp/test/InsertDemo_03.java
+++ JavaCodes/src/in/sp/test/InsertDemo_03.java
@@ -4,24 +4,35 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 
-public class InsertDemo_01 {
+public class InsertDemo_03 {
 	
 	public static void main(String[] args) throws Exception {
+		
+		String name1 = "Amit";
+		String mail1 = "amit@gmail.com";
+		String password1 = "505060";
+		String gender1 = "male";
+		String city1 = "Bengluru";
 		
 		Class.forName("com.mysql.cj.jdbc.Driver");
 		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc_db", "root", "802152");
 	    
-		PreparedStatement ps = con.prepareStatement("insert into register values('manish', 'manish@gmail.com', '12345', 'male', 'dibrugarh')");
+		PreparedStatement ps = con.prepareStatement(
+				"INSERT INTO register VALUES (?,?,?,?,?)");
+		ps.setString(1, name1);
+		ps.setString(2, mail1);
+		ps.setString(3, password1);
+		ps.setString(4, gender1);
+		ps.setString(5, city1);
+		
 		int i = ps.executeUpdate();
 		
 		if(i > 0) {
 			System.out.println("Success");
-		}else {
+		}
+		else {
 			System.out.println("Fail");
 		}
-		
-		ps.close();
-		con.close();
 	}
 }
   
