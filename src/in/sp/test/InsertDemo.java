4ae5e8096069ae5f7c6dc1e3ae9ef12c4bdb7211
manish-t01 <manish9366801212@gmail.com>
2026-07-12 21:16:16 +0530

    Created Project 'jdbcInsertDemo'

---
+++ src/in/sp/test/InsertDemo.java
@@ -0,0 +1,14 @@
+package in.sp.test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class InsertDemo {
+	public static void main(String[] args) throws Exception {
+		
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc_db", "root", "802152");
+	    System.out.print("Success.");
+	}
+}
+  
