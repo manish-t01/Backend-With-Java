56217756b31f2d234743a6924c36f752d0dc9383
manish-t01 <manish9366801212@gmail.com>
2026-07-16 03:30:50 +0530

    fetch code

--- Advance JDBC/JDBC DEMO/src/jdbc/learning/Main_01.java
+++ Advance JDBC/JDBC DEMO/src/jdbc/learning/Main_01.java
@@ -3,9 +3,39 @@ package jdbc.learning;
 import java.sql.*;
 
 public class Main_01 {
+
+    private static String url = "jdbc:mysql://localhost:3306/mydb";
+    private static String user = "root";
+    private static String password = "802152";
+
     public static void main(String[] args) {
         try{
-            
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        }catch(ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        try{
+
+            Connection con = DriverManager.getConnection(url, user, password);
+            Statement st = con.createStatement();
+            String query = "select * from students";
+            ResultSet rs = st.executeQuery(query);
+
+            while (rs.next()) {
+                int id = rs.getInt(  "id");
+                String name = rs.getString(  "name");
+                int age = rs.getInt(  "age");
+                double marks = rs.getDouble(  "marks");
+                System.out.println(id + " | " + name + " | " + age + " | " + marks);
+            }
+
+            rs.close();
+            st.close();
+            con.close();
+
+        }catch(SQLException e){
+            e.printStackTrace();
         }
     }
 }
