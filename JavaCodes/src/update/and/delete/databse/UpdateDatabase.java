56f8363b08f2750b0d66824bdc73a00adaf8b4b8
manish-t01 <manish9366801212@gmail.com>
2026-07-15 17:04:27 +0530

    update in database code

---
+++ JavaCodes/src/update/and/delete/databse/UpdateDatabase.java
@@ -0,0 +1,41 @@
+package update.and.delete.databse;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.util.Scanner;
+
+public class UpdateDatabase {
+	
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Updating Your City ");
+		System.out.println("Enter Your City Name ");
+		String city2 = sc.nextLine();
+		
+		System.out.println("Enter Your Email ");
+		String email2 = sc.nextLine();
+		
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc_db", "root", "802152");
+	    
+		PreparedStatement ps = con.prepareStatement(
+				"Update register set email = ? where city = ?");
+		
+		ps.setString(1, city2);
+		ps.setString(2, email2);
+		
+		int i = ps.executeUpdate();
+		
+		if(i > 0) {
+			System.out.println("Update Successful");
+		}else {
+			System.out.println("Update Failed");
+		}
+		
+		ps.close();
+		con.close();
+	}
+}
+  
