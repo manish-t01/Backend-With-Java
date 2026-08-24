21132b2d559b95cd4bd7883f28ef2d45b3c4062f
manish-t01 <manish9366801212@gmail.com>
2026-08-24 23:59:47 +0530

    Project initialized

--- Servlet/JavaCodes/project 01/src/main/java/com/backend/MyServlet.java
+++ Servlet/JavaCodes/project 01/src/main/java/com/backend/MyServlet.java
@@ -1,5 +1,19 @@
 package com.backend;
 
-public class MyServlet {
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class MyServlet extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest reqest, HttpServletResponse response) throws ServletException, IOException {
+		
+		// Backend Code
+		
+	}
 
 }
