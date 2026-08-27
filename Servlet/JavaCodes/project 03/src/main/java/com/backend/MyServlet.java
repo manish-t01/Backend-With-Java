c98edde7cbc55ea98cb929a75a5d5c3f272b72ee
manish-t01 <manish9366801212@gmail.com>
2026-08-28 00:35:14 +0530

    Servlet Annotations Configurations || @WebServlet Annotation || No need of web.xml

--- Servlet/JavaCodes/project 03/src/main/java/com/backend/MyServlet.java
+++ Servlet/JavaCodes/project 03/src/main/java/com/backend/MyServlet.java
@@ -1,5 +1,25 @@
 package com.backend;
 
-public class MyServlet {
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/mypage")
+public class MyServlet extends HttpServlet {
+	
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		PrintWriter out = response.getWriter();
+		out.print("Hi, You are Welcome!");
+		
+		System.out.println("I am in service method.");
+		
+	}
 
 }
