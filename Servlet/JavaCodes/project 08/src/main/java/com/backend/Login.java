03cc5d5a7427ff4bd877b3a6ee8d59b34d485744
manish-t01 <manish9366801212@gmail.com>
2026-08-30 01:03:38 +0530

    Learned RequestDispatcher

--- Servlet/JavaCodes/project 08/src/main/java/com/backend/Login.java
+++ Servlet/JavaCodes/project 08/src/main/java/com/backend/Login.java
@@ -1,5 +1,40 @@
 package com.backend;
 
-public class Login {
+import java.io.IOException;
+import java.io.PrintWriter;
 
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        String myemail = req.getParameter("email1");
+        String mypass = req.getParameter("pass1");
+
+        PrintWriter out = resp.getWriter();
+
+        if (myemail.equals("manish@gmail.com") && mypass.equals("manish123")) {
+
+            RequestDispatcher rd = req.getRequestDispatcher("/profile.jsp");
+            rd.forward(req, resp);
+
+        } else {
+
+            resp.setContentType("text/html");
+
+            out.print("<h3 style='color:red'>Email id and password didn't match</h3>");
+
+            RequestDispatcher rd = req.getRequestDispatcher("/index.html");
+            rd.include(req, resp);
+        }
+    }
 }
